9a991556d4156d0d7dacb57efa2eae67694da9c6
mickeysanchez <sanchez.mickey@gmail.com>
2014-01-29 18:12:05 -0500

    starts looking at sockets

---
+++ tcp-client.rb
@@ -0,0 +1,13 @@
+require 'socket'
+
+loop do
+  s = TCPSocket.new 'localhost', 2000
+  message = nil
+  while message.nil?
+    message = s.gets  # Read lines from socket
+    puts message      # and print them
+    message = nil
+  end
+  s.close
+end
+
